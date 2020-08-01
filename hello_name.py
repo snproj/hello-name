1f759bf309e7f8c2a62369acb15c416e63d8dcf5
Sunrise <66585571+snproj@users.noreply.github.com>
2020-08-02 02:24:27 +0800

    Create hello_name.py

---
+++ hello_name.py
@@ -0,0 +1,18 @@
+name1 = "John"
+name2 = "Mike"
+name3 = "Sarah"
+name4 = "Thomas"
+name5 = "Jennifer"
+
+def hello_name_function(name):
+  print ("Hello, " + name + ", nice to meet you.")
+  input()
+
+hello_name_function(name1)
+hello_name_function(name2)
+hello_name_function(name3)
+hello_name_function(name4)
+hello_name_function(name5)
+
+print("Press ENTER to exit program.")
+input()
